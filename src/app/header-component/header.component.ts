3ecd0634709e582c0eaa9af47c2ca823dbbff2d3
Mesay Bekele <me2say@gmail.com>
2020-10-27 22:40:41 -0400

    created componenets needed to build the app

---
+++ src/app/header-component/header.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: 'recipeShop-header',
+    templateUrl: "./header.component.html",
+    styleUrls: ["./header.component.css"],
+
+})
+
+export class RecipeShopHeader {
+    title ='mesay'
+
+}
---
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe-book.model'
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('Test Recipe', 'Test Desc', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg' ), new Recipe('Test Recipe', 'Test Desc', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg' )]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

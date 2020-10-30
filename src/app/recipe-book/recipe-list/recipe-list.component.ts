5f2ee43ebb749d0b26eaddd73cad2d8860b91c92
Mesay Bekele <me2say@gmail.com>
2020-10-30 00:10:16 -0400

    enabled navigation between recipe and shopping

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from '../recipe-book.model'
+import { Recipe } from '../recipe-book.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [new Recipe('Test Recipe', 'Test Desc', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg' ), new Recipe('Test Recipe', 'Test Desc', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg' )]
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe1',
+      'Test Desc1',
+      'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-1.jpg'
+    ),
+    new Recipe(
+      'Test Recipe2',
+      'Test Desc2',
+      'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'
+    ),
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
